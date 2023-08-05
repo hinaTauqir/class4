bf8b0394560b47be97fd1826280334df6d14f773
hinaTauqir <139550018+hinaTauqir@users.noreply.github.com>
2023-08-05 21:20:48 +0500

    update

--- class4.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.g = void 0;
-//loop
-// forloop
-let f = 5;
-let g = 9;
-exports.g = g;
-exports.default = f;
--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 import anynumber from "./class4";
 import {g} from "./class4";
-console.log(anynumber);
+//console.log(anynumber);
 //commonjs
 // let g = require("./class4") for less version es06
